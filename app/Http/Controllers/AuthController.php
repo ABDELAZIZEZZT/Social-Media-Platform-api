ecc5c1548f68cd42c563ffe395d8e5e493d8faa0
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2024-08-06 18:45:15 +0300

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+// use Exception;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+use function Laravel\Prompts\password;
+
+class AuthController extends Controller
+{
+    //
+    function registration(Request $request){
+
+        $validator= Validator::make($request->all(),[
+            'first_name'=>'required|max:255',
+            'last_name'=>'required|max:255',
+            'email'=>'required|email|max:255|unique:users',
+            'password'=>['required','confirmed',
+                        Password::min('8')
+                        ->letters()
+                        ->mixedCase()
+                        ->numbers()
+                        ->symbols(),
+                    ],
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+        if($user){
+            return response()->json([
+                'message' => 'User registered successfully',
+                'user' => $user,
+                'token' => $token,
+            ]);
+        }
+
+
+
+
+    }
+    function login(Request $request){
+        $validator= Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>['required',
+                        Password::min('8')
+                        ->letters()
+                        ->mixedCase()
+                        ->numbers()
+                        ->symbols(),],
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $credentials = $request->only('email', 'password');
+        $token = JWTAuth::attempt($credentials);
+        try {
+            if (! $token) {
+                return response()->json([
+                    'message' => 'Invalid email or password',
+                ], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'Could not create token',
+            ]);
+        }
+
+        $user=Auth::user();
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => $user,
+            'token' => $token,
+        ]);
+
+    }
+
+    function logout(Request $request){
+        // dd($request->all());
+        try {
+            // Invalidate the current token
+            JWTAuth::parseToken()->invalidate();
+
+            return response()->json([
+                'message' => 'User logged out successfully',
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'Failed to logout, token invalid.',
+            ], 400);
+        }
+    }
+
+}
+
---
+++ app/Http/Middleware/verifyToken.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class verifyToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $token=$request->token;
+            if($token){
+                $user=JWTAuth::parseToken()->Authenticate();
+                return $next($request);
+            }else{
+                return response()->json(
+                    ['messag' => 'token is requaired']
+                );
+            }
+
+        } catch (JWTException $e) {
+           if($e instanceof TokenInvalidException){
+            return response()->json(
+                ['messag' => 'token is invalid']
+            );
+           }else if($e instanceof TokenExpiredException){
+            return response()->json(
+                ['messag' => 'token is expaired']
+            );
+           }else{
+            return response()->json(
+                ['messag' => 'exaption']
+            );
+           }
+        }
+    }
+}
