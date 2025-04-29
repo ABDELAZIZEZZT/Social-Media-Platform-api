68f5df60d92614b3aa21ae300a4cb154802d75ca
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2025-04-29 05:26:25 +0300

    refactor: the Auth methods refactoring.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,15 +10,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Validation\Rules\Password;
 use Symfony\Component\HttpFoundation\Response;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Illuminate\Support\Str;
-use Illuminate\Validation\ValidationException as ValidationValidationException;
 use Laravel\Socialite\Facades\Socialite;
 
 use function Laravel\Prompts\password;
